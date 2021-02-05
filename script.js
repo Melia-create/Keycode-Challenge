f93f644492463579848f355270700e27f1321b8d
Amelia <amelia.riding@Sofology.co.uk>
2021-02-05 11:44:15 +0000

    #3 Key code added

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.addEventListener("keydown", function (event){
     let pressKey = document.querySelector("#which");
-    pressKey.textContent = `${event}.which`;
+    pressKey.textContent = `${event}.which`;    
+    let eventKey = document.querySelector("#eventkey");
+    eventKey.textContent = `${event.key}`;
+    let eventCode = document.querySelector ("#eventcode")
+    eventCode.textContent = `Key${event.key}`
+});
