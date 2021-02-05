a0b2463d5087e94156fdcf817b71ea399ef96052
Amelia <amelia.riding@Sofology.co.uk>
2021-02-05 14:30:08 +0000

    Solved alignment issue, need to hide event headers

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+let hKey = document.querySelector("#hkey")
+let hWhich = document.querySelector("#hwhich")
+let hCode = document.querySelector("#hcode")
+
 window.addEventListener("keydown", function (event){
-    let pressKey = document.querySelector("#which");
-    pressKey.textContent = `${event}.which`;    
+    let pressKey = document.querySelector("#presskey");
+    pressKey.textContent = event.which;    
     let eventKey = document.querySelector("#eventkey");
-    eventKey.textContent = `${event.key}`;
+    eventKey.textContent = event.key;
     let eventCode = document.querySelector ("#eventcode")
-    eventCode.textContent = `Key${event.key}`
+    eventCode.textContent = event.code
+    let eventWhich = document.querySelector ("#eventwhich")
+    eventWhich.textContent = event.which
 });
+
